97a098b513151cafb78ed009e9b9b7b99b7c7c85
Phat Ho <phat.h.ho@dektech.com.au>
2018-09-11 11:51:10 +0700

    update login api

--- server.js
+++ server.js
@@ -8,7 +8,33 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use('/api', router);
 server.use(middlewares);
+server.use(jsonServer.defaults)
 server.use(router);
+
+// login get output modifying
+router.render = function (req, res) {
+  let resData = res.locals.data;
+	console.log(req.url);
+  if (req.url.match(/\/login.*/)) {
+  	
+  	if (resData.length == 1){
+  		resData = resData[0];
+  		res.jsonp({
+  			userVerified: true,
+  			userRole: resData.role
+    	});
+  	}
+  	else{
+  		res.jsonp({
+  			userVerified: false
+    	});
+  	}
+    
+  } else {
+    res.jsonp(res.locals.data)
+  }
+}
 
 server.listen(port);
